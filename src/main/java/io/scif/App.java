140fd4fcf106bf34886f89b7505339e05da2f7fd
Elliott Magnuson <ecmagnuson@wisc.edu>
2022-09-27 08:45:00 -0500

    added class that extends AbstractService

--- src/main/java/io/scif/App.java
+++ src/main/java/io/scif/App.java
@@ -1,13 +1,11 @@
 package io.scif;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
+import org.scijava.service.AbstractService;
+
+public class App extends AbstractService{
+
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
+
 }
